50afe564186de76a5ed693d15d885f9c4ddba085
StefanHerald <s.herald@students.uu.nl>
2022-12-06 13:21:27 +0100

    small change

--- search.py
+++ search.py
@@ -97,6 +97,7 @@ def breadthFirstSearch(problem: SearchProblem):
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    "NO"
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
